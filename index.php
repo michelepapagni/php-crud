7101c11c78fbfbaaae31d505948fe885aeff969b
Michele Papagni <michele_papagni@me.com>
2019-02-07 09:51:58 +0100

    scaffolding

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>La prima CRUD</title>
+        <link rel="stylesheet" href="public/css/app.css">
+    </head>
+    <body>
+        <div class="container">
+            Ciao
+        </div>
+    </body>
+</html>
